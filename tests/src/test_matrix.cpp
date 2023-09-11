2985ecb1687279a5425461d3f433ab7c7120c922
Manuel <m-tischhauser@hotmail.com>
2023-09-11 20:58:57 +0200

    updated

--- tests/src/test_matrix.cpp
+++ tests/src/test_matrix.cpp
@@ -5,6 +5,8 @@
 int main() {
     std::cout << "test main" << std::endl;
 
-    auto matrix = linalg::Matrix();
+    auto matrix = linalg::Matrix<float, 3, 4>({{1 ,2, 3, 4},
+                                               {1, 2, 3, 4},
+                                               {1, 2 ,3, 4}});
     return 0;
 }
