0738857b8de3811dcda4d22c047ef1c862c6a2c5
Manuel <m-tischhauser@hotmail.com>
2023-09-13 21:21:52 +0200

    updated

--- benchmarks/cpp/src/benchmarks.cpp
+++ benchmarks/cpp/src/benchmarks.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+#include "matrix_multiplications.h"
+
 int main() {
     std::cout << "benchmarks" << std::endl;
+
+    auto A = linalg::Matrix<int, 2, 3>{{1, 2, 3},
+                                       {4, 5, 6}};
+
+    auto B = linalg::Matrix<int, 3, 2>{{10, 11},
+                                       {12, 13},
+                                       {14, 15}};
+
+    auto C = matmul::simple(A, B);
+
+    std::cout << "Result: " << std::endl
+              << C(0, 0) << " " << C(0, 1) << " " << C(0, 2) << std::endl
+              << C(1, 0) << " " << C(1, 1) << " " << C(1, 2) << std::endl;
 
     return 0;
 }
