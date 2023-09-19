30df6def5f2e4c40f6a68190a6ceb6b93b2da5d5
Manuel <m-tischhauser@hotmail.com>
2023-09-19 22:12:41 +0200

    test for numpy matrix multiplication added

--- benchmarks/python/numpy_matrix.py
+++ benchmarks/python/numpy_matrix.py
@@ -1,1 +1,12 @@
+import time
 import numpy as np
+
+if __name__ == '__main__':
+    A = np.random.rand(1024, 1024)
+    B = np.random.rand(1024, 1024)
+    
+    t_ref = time.time()
+
+    C = A@B
+
+    print('Elapsed time = {:} ms'.format((time.time() - t_ref) * 1000))
