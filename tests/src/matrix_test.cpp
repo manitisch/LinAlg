da58920be8ff4da6074d48ad2edf2ea429f227b3
Manuel <m-tischhauser@hotmail.com>
2023-09-19 22:10:04 +0200

    updated

--- tests/src/matrix_test.cpp
+++ tests/src/matrix_test.cpp
@@ -4,15 +4,15 @@
 #include "matrix.h"
 
 TEST_CASE("simple test") {
-    auto matrix = linalg::Matrix<float, 2, 3>{{{1 ,2, 3},
-                                               {4, 5, 6}}};
+    linalg::Matrix<float, 2, 3> A{{{1 ,2, 3},
+                                   {4, 5, 6}}};
 
-    CHECK(1 == matrix(0, 0));
-    CHECK(2 == matrix(0, 1));
-    CHECK(3 == matrix(0, 2));
-    CHECK(4 == matrix(1, 0));
-    CHECK(5 == matrix(1, 1));
-    CHECK(6 == matrix(1, 2));
+    CHECK(1 == A(0, 0));
+    CHECK(2 == A(0, 1));
+    CHECK(3 == A(0, 2));
+    CHECK(4 == A(1, 0));
+    CHECK(5 == A(1, 1));
+    CHECK(6 == A(1, 2));
 }
 
 TEST_CASE("wrong size of initializer lists") {
