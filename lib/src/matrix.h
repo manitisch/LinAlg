6c66a247093d9696f7cc341733e41e727b1657d1
Manuel <m-tischhauser@hotmail.com>
2023-09-20 17:06:06 +0200

    interface for iterators added

--- lib/src/matrix.h
+++ lib/src/matrix.h
@@ -2,6 +2,7 @@
 #define LINALG_MATRIX_H
 
 #include <array>
+#include <iterator>
 #include <stdexcept>
 #include <tuple>
 	
@@ -10,10 +11,39 @@ namespace linalg {
 template<typename T, size_t Rows, size_t Cols>
 class Matrix {
   public:
+    using RowIterator = std::array<T, Cols>::iterator;
+
+    class ColumnIterator {
+      public:
+        using difference_type   = std::ptrdiff_t;
+        using value_type        = T;
+        using pointer           = T*;
+        using reference         = T&;
+    
+        ColumnIterator();
+        ColumnIterator(pointer ptr) : ptr_(ptr) {}
+
+        reference operator*() const { /*return *ptr_;*/ }
+        pointer operator->() { /*return ptr_; */}
+        ColumnIterator& operator++() { /*ptr_++; return *this;*/ }  
+        ColumnIterator operator++(int) {/*ColumnIterator tmp = *this; ++(*this); return tmp;*/ }
+        friend bool operator== (const ColumnIterator& a, const ColumnIterator& b) { /*return a.ptr_ == b.ptr_; */}
+        //friend bool operator!= (const ColumnIterator& a, const ColumnIterator& b) { /*return a.ptr_ != b.ptr_; */} // not needed
+
+      private:
+        pointer ptr_;
+        
+    };
+
     Matrix();
     Matrix(std::initializer_list<std::initializer_list<T>> values);
 
     T& operator()(const size_t row, const size_t col);
+
+    RowIterator rwiseBegin(const size_t row);
+    RowIterator rwiseEnd(const size_t row);
+    ColumnIterator cwiseBegin(const size_t col);
+    ColumnIterator cwiseEnd(const size_t col);
 
     std::array<T, Cols> getRow(const size_t row);
     std::array<T, Rows> getCol(const size_t col);
@@ -21,6 +51,10 @@ class Matrix {
 
   private:
     std::array<std::array<T, Cols>, Rows> data_;
+
+    static_assert(std::forward_iterator<RowIterator>);
+    static_assert(std::forward_iterator<ColumnIterator>);
+    ColumnIterator columnIterator = ColumnIterator(nullptr);
 };
 
 template<typename T, size_t Rows, size_t Cols>
@@ -48,8 +82,31 @@ Matrix<T, Rows, Cols>::Matrix(std::initializer_list<std::initializer_list<T>> va
 
 template<typename T, size_t Rows, size_t Cols>
 T& Matrix<T, Rows, Cols>::operator()(const size_t row, const size_t col) {
+    // TODO check bounds
     return data_[row][col];
 };
+
+template<typename T, size_t Rows, size_t Cols>
+Matrix<T, Rows, Cols>::RowIterator Matrix<T, Rows, Cols>::rwiseBegin(const size_t row) {
+    // TODO check bounds
+    return data_[row].begin();
+}
+
+template<typename T, size_t Rows, size_t Cols>
+Matrix<T, Rows, Cols>::RowIterator Matrix<T, Rows, Cols>::rwiseEnd(const size_t row) {
+    // TODO check bounds
+    return data_[row].end();
+}
+
+template<typename T, size_t Rows, size_t Cols>
+Matrix<T, Rows, Cols>::ColumnIterator Matrix<T, Rows, Cols>::cwiseBegin(const size_t col) {
+    // TODO
+}
+
+template<typename T, size_t Rows, size_t Cols>
+Matrix<T, Rows, Cols>::ColumnIterator Matrix<T, Rows, Cols>::cwiseEnd(const size_t col) {
+    // TODO
+}
 
 template<typename T, size_t Rows, size_t Cols>
 std::array<T, Cols> Matrix<T, Rows, Cols>::getRow(const size_t row) {
