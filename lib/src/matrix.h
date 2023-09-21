b91875376f3f7386eaf7a801c950fca491478e2e
Manuel <m-tischhauser@hotmail.com>
2023-09-21 21:04:57 +0200

    column iterator implemented

--- lib/src/matrix.h
+++ lib/src/matrix.h
@@ -8,63 +8,79 @@
 	
 namespace linalg {
     
-template<typename T, size_t Rows, size_t Cols>
+template<typename T, size_t Rows, size_t Columns>
 class Matrix {
   public:
-    using RowIterator = std::array<T, Cols>::iterator;
+    using RowIterator = std::array<T, Columns>::iterator;
 
     class ColumnIterator {
       public:
-        using difference_type   = std::ptrdiff_t;
-        using value_type        = T;
-        using pointer           = T*;
-        using reference         = T&;
+        using difference_type  = ptrdiff_t;
+        using value_type = T;
+        using pointer = T*;
+        using reference = T&;
     
         ColumnIterator();
-        ColumnIterator(pointer ptr) : ptr_(ptr) {}
+        ColumnIterator(pointer ptr) : ptr_(ptr) {};
 
-        reference operator*() const { /*return *ptr_;*/ }
-        pointer operator->() { /*return ptr_; */}
-        ColumnIterator& operator++() { /*ptr_++; return *this;*/ }  
-        ColumnIterator operator++(int) {/*ColumnIterator tmp = *this; ++(*this); return tmp;*/ }
-        friend bool operator== (const ColumnIterator& a, const ColumnIterator& b) { /*return a.ptr_ == b.ptr_; */}
-        //friend bool operator!= (const ColumnIterator& a, const ColumnIterator& b) { /*return a.ptr_ != b.ptr_; */} // not needed
+        reference operator*() const {
+            return *ptr_;
+        };
 
+        pointer operator->() {
+            return ptr_;
+        };
+        
+        ColumnIterator& operator++() {
+            ptr_ += Columns;
+            return *this;
+        };
+
+        ColumnIterator operator++(int) {
+            ColumnIterator tmp = *this;
+            ++(*this);
+            return tmp;
+        };
+
+        friend bool operator== (const ColumnIterator& left, const ColumnIterator& right) {
+            return left.ptr_ == right.ptr_;
+        };
+        
       private:
         pointer ptr_;
-        
     };
 
     Matrix();
     Matrix(std::initializer_list<std::initializer_list<T>> values);
 
-    T& operator()(const size_t row, const size_t col);
+    T& operator()(const size_t row, const size_t column);
 
     RowIterator rwiseBegin(const size_t row);
     RowIterator rwiseEnd(const size_t row);
-    ColumnIterator cwiseBegin(const size_t col);
-    ColumnIterator cwiseEnd(const size_t col);
+    ColumnIterator cwiseBegin(const size_t column);
+    ColumnIterator cwiseEnd(const size_t column);
 
-    std::array<T, Cols> getRow(const size_t row);
-    std::array<T, Rows> getCol(const size_t col);
+    std::array<T, Columns> getRow(const size_t row);
+    std::array<T, Rows> getCol(const size_t column);
     const std::tuple<const size_t, const size_t> shape();
 
   private:
-    std::array<std::array<T, Cols>, Rows> data_;
+    std::array<std::array<T, Columns>, Rows> data_;
 
     static_assert(std::forward_iterator<RowIterator>);
     static_assert(std::forward_iterator<ColumnIterator>);
-    ColumnIterator columnIterator = ColumnIterator(nullptr);
+    ColumnIterator columnIterator = ColumnIterator(data_.front().data());
 };
 
-template<typename T, size_t Rows, size_t Cols>
-Matrix<T, Rows, Cols>::Matrix() {
+
+template<typename T, size_t Rows, size_t Columns>
+Matrix<T, Rows, Columns>::Matrix() {
     data_.fill({});
 };
 
-template<typename T, size_t Rows, size_t Cols>
-Matrix<T, Rows, Cols>::Matrix(std::initializer_list<std::initializer_list<T>> values) {
-    if(Rows != values.size() || Cols != values.begin()->size()) {
+template<typename T, size_t Rows, size_t Columns>
+Matrix<T, Rows, Columns>::Matrix(std::initializer_list<std::initializer_list<T>> values) {
+    if(Rows != values.size() || Columns != values.begin()->size()) {
         throw std::invalid_argument("Size of initializer list does not match matrix dimensions.");
     }
 
@@ -80,53 +96,67 @@ Matrix<T, Rows, Cols>::Matrix(std::initializer_list<std::initializer_list<T>> va
     }
 };
 
-template<typename T, size_t Rows, size_t Cols>
-T& Matrix<T, Rows, Cols>::operator()(const size_t row, const size_t col) {
+template<typename T, size_t Rows, size_t Columns>
+T& Matrix<T, Rows, Columns>::operator()(const size_t row, const size_t column) {
     // TODO check bounds
-    return data_[row][col];
+    return data_[row][column];
 };
 
-template<typename T, size_t Rows, size_t Cols>
-Matrix<T, Rows, Cols>::RowIterator Matrix<T, Rows, Cols>::rwiseBegin(const size_t row) {
-    // TODO check bounds
+template<typename T, size_t Rows, size_t Columns>
+Matrix<T, Rows, Columns>::RowIterator Matrix<T, Rows, Columns>::rwiseBegin(const size_t row) {
+    if (Rows >= row) {
+        throw std::invalid_argument("Row " + std::string{row} + " outside range.");
+    }
+
     return data_[row].begin();
 }
 
-template<typename T, size_t Rows, size_t Cols>
-Matrix<T, Rows, Cols>::RowIterator Matrix<T, Rows, Cols>::rwiseEnd(const size_t row) {
-    // TODO check bounds
+template<typename T, size_t Rows, size_t Columns>
+Matrix<T, Rows, Columns>::RowIterator Matrix<T, Rows, Columns>::rwiseEnd(const size_t row) {
+    if (Rows >= row) {
+        throw std::invalid_argument("Row " + std::string{row} + " outside range.");
+    }
+
     return data_[row].end();
 }
 
-template<typename T, size_t Rows, size_t Cols>
-Matrix<T, Rows, Cols>::ColumnIterator Matrix<T, Rows, Cols>::cwiseBegin(const size_t col) {
-    // TODO
+template<typename T, size_t Rows, size_t Columns>
+Matrix<T, Rows, Columns>::ColumnIterator Matrix<T, Rows, Columns>::cwiseBegin(const size_t column) {
+    if (Columns >= column) {
+        throw std::invalid_argument("Column " + std::string{column} + " outside range.");
+    }
+
+    return ColumnIterator{data_.front().data() + column};
 }
 
-template<typename T, size_t Rows, size_t Cols>
-Matrix<T, Rows, Cols>::ColumnIterator Matrix<T, Rows, Cols>::cwiseEnd(const size_t col) {
-    // TODO
+template<typename T, size_t Rows, size_t Columns>
+Matrix<T, Rows, Columns>::ColumnIterator Matrix<T, Rows, Columns>::cwiseEnd(const size_t column) {
+    if (Columns >= column) {
+        throw std::invalid_argument("Column " + std::string{column} + " outside range.");
+    }
+
+    return ColumnIterator{data_.front().data() + Rows + column};
 }
 
-template<typename T, size_t Rows, size_t Cols>
-std::array<T, Cols> Matrix<T, Rows, Cols>::getRow(const size_t row) {
+template<typename T, size_t Rows, size_t Columns>
+std::array<T, Columns> Matrix<T, Rows, Columns>::getRow(const size_t row) {
     return data_[row];
 }
 
-template<typename T, size_t Rows, size_t Cols>
-std::array<T, Rows> Matrix<T, Rows, Cols>::getCol(const size_t col) {
-    std::array<T, Rows> column{};
+template<typename T, size_t Rows, size_t Columns>
+std::array<T, Rows> Matrix<T, Rows, Columns>::getCol(const size_t column) {
+    std::array<T, Rows> tmp_column{};
 
     for (size_t i = 0; i < Rows; i++) {
-        column[i] = data_[i][col];
+        tmp_column[i] = data_[i][column];
     }
 
-    return column;
+    return tmp_column;
 }
 
-template<typename T, size_t Rows, size_t Cols>
-const std::tuple<const size_t, const size_t> Matrix<T, Rows, Cols>::shape() {
-    return {Rows, Cols};
+template<typename T, size_t Rows, size_t Columns>
+const std::tuple<const size_t, const size_t> Matrix<T, Rows, Columns>::shape() {
+    return {Rows, Columns};
 }
 
 } // namespace linalg
